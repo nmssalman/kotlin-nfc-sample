e073738da95b797578a1e529c09bd40c6e0fd33a
nmssalman <nmssalman@outlook.com>
2019-12-21 11:38:17 +0900

    nfc reader full code

---
+++ app/src/main/java/com/salman/nfcreader/MainActivity.kt
@@ -0,0 +1,85 @@
+package com.salman.nfcreader
+
+import android.app.PendingIntent
+import android.content.Intent
+import android.content.IntentFilter
+import android.nfc.NdefMessage
+import android.nfc.NfcAdapter
+import android.nfc.tech.NfcF
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.Settings
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+
+
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    private var intentFiltersArray: Array<IntentFilter>? = null
+    private val techListsArray = arrayOf(arrayOf(NfcF::class.java.name))
+    private val nfcAdapter: NfcAdapter? by lazy {
+        NfcAdapter.getDefaultAdapter(this)
+    }
+    private var pendingIntent: PendingIntent? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //nfc process start
+        pendingIntent = PendingIntent.getActivity(
+            this, 0, Intent(this, javaClass).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0
+        )
+        val ndef = IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED)
+        try {
+            ndef.addDataType("text/plain")
+        } catch (e: IntentFilter.MalformedMimeTypeException) {
+            throw RuntimeException("fail", e)
+        }
+        intentFiltersArray = arrayOf(ndef)
+        if (nfcAdapter == null) {
+            val builder = AlertDialog.Builder(this@MainActivity, R.style.MyAlertDialogStyle)
+            builder.setMessage("Not Eligible for this Device")
+            builder.setPositiveButton("Cancel", null)
+        } else if (!nfcAdapter!!.isEnabled) {
+            val builder = AlertDialog.Builder(this@MainActivity, R.style.MyAlertDialogStyle)
+            builder.setTitle("NFC Disabled")
+            builder.setMessage("Plesae Enable NFC")
+            builder.setPositiveButton("Settings") { _, _ -> startActivity(Intent(Settings.ACTION_NFC_SETTINGS)) }
+            builder.setNegativeButton("Cancel", null)
+            val myDialog = builder.create()
+            myDialog.setCanceledOnTouchOutside(false)
+            myDialog.show()
+        }
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        nfcAdapter?.enableForegroundDispatch(this, pendingIntent, intentFiltersArray, techListsArray)
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        val action = intent.action
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED == action) {
+            val parcelables = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES)
+            with(parcelables) {
+                val inNdefMessage = this[0] as NdefMessage
+                val inNdefRecords = inNdefMessage.records
+                val ndefRecord_0 = inNdefRecords[0]
+                val inMessage = String(ndefRecord_0.payload)
+                txttext.setText(inMessage);
+            }
+        }
+    }
+
+    override fun onPause() {
+        if (this.isFinishing) {
+            nfcAdapter?.disableForegroundDispatch(this)
+        }
+        super.onPause()
+    }
+}
